df237a1e2f7fafbe1af98c2a1ed9a0ad8f498a60
ekwayv8 <yvon.ekwalla@gmail.com>
2019-09-12 21:40:54 +0200

    Adding a factoryBot file for admins

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :admin do
-    
+  	number { Faker::PhoneNumber.phone_number_with_country_code }
+    password { "password1" }
+    firstname { Faker::Name.first_name }
+    lastname { Faker::Name.last_name }
+    function { "CEO" }
+    email { Faker::Internet.email }
+    country { FactoryBot.create(:country) }
+
   end
 end
