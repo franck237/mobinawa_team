3e45701df5805b4633d2770da4d7e9050927bd19
franck237 <franck.kamanou@gmail.com>
2019-09-13 22:32:44 +0200

    Index view for Sector

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,52 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//
+// Add of Javascript used by Boomerang
+//
+// <!-- Core -->
+//= require jquery/jquery.min
+//= require popper/popper.min
+//= require bootstrap/js/bootstrap.min
+//= require slidebar/slidebar
+//= require classie
+//= require revolution/revolution-slider-1
+//
+// <!-- Bootstrap Extensions -->
+//= require bootstrap-notify/bootstrap-growl.min
+//= require scrollpos-styler/scrollpos-styler
+//
+// <!-- Plugins: Sorted A-Z -->
+//= require adaptive-backgrounds/adaptive-backgrounds
+//= require countdown/js/jquery.countdown.min
+//= require dropzone/dropzone.min
+//= require easy-pie-chart/jquery.easypiechart.min
+//= require fancybox/js/jquery.fancybox.min
+//= require flatpickr/flatpickr.min
+//= require flip/flip.min
+//= require footer-reveal/footer-reveal.min
+//= require gradientify/jquery.gradientify.min
+//= require headroom/headroom.min
+//= require headroom/jquery.headroom.min
+//= require input-mask/input-mask.min
+//= require instafeed/instafeed
+//= require milestone-counter/jquery.countTo
+//= require nouislider/js/nouislider.min
+//= require paraxify/paraxify.min
+//= require select2/js/select2.min
+//= require sticky-kit/sticky-kit.min
+//= require swiper/js/swiper.min
+//= require textarea-autosize/autosize.min
+//= require typeahead/typeahead.bundle.min
+//= require typed/typed.min
+//= require vide/vide.min
+//= require viewport-checker/viewportchecker.min
+//= require wow/wow.min
+//
+// <!-- Isotope -->
+//= require isotope/isotope.min
+//= require imagesloaded/imagesloaded.pkgd.min
+//
+// <!-- App JS -->
+//= require boomerang.min
+
