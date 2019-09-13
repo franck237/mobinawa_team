08c31b29e7803b346657ca46ee637f76a76362a5
ekwayv8 <yvon.ekwalla@gmail.com>
2019-09-13 11:00:20 +0200

    Modification of the routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,19 +5,19 @@ Rails.application.routes.draw do
 
 
   resources :admins do
-  	resources :compagnies do
+  	resources :companies do
   		resources :products
   	end
   end
 
- resources :countries do
-   resources :sectors do
-  	resources :sub_sectors do
-  	  resources :compagnies do
+  resources :countries do
+    resources :sectors do
+  	 resources :sub_sectors do
+  	  resources :companies do
   	 	resources :products
   	  end
-  	end
-   end
- end
+  	 end
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
